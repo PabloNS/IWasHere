3fd503856e0870669b388240c295725ad690c259
Nunez Serra <pnunezserra@deloitte.es>
2023-04-14 10:43:42 +0200

    Log

--- src/main/java/com/example/IWasHere/controller/NoteController.java
+++ src/main/java/com/example/IWasHere/controller/NoteController.java
@@ -6,18 +6,21 @@ import com.example.IWasHere.dto.NotesDTO;
 import com.example.IWasHere.service.NoteService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("notes")
 @AllArgsConstructor
 @CrossOrigin("*")
+@Slf4j
 public class NoteController {
 
     private NoteService noteService;
 
     @PostMapping
     public void createNote(@RequestBody NoteDTO note, HttpServletRequest request) {
+        log.info("CreateNote invoked! " + note);
         noteService.createNote(note, request);
     }
 
